e8893af03fc37898960a579955db1d286e9f0777
Chaotic <34907865+GageGamesHD@users.noreply.github.com>
2018-05-21 20:39:17 -0700

    Added all files
    Made plugin complete

---
+++ src/GageGamesHD/AlwaysSpawn/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GageGamesHD\AlwaysSpawn;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\plugin\PluginBase as Plugin;
+
+class Loader extends Plugin implements Listener{
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getLogger()->info(&c"Auto Spawn By GageGamesHD has been enabled!");
+	}
+
+	public function onPlayerLogin(PlayerLoginEvent $event){
+		$event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+	}
+
+}
